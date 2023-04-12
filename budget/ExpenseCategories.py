b0815b90bba7c62dfdf2b0146d1fae19df9324dd
osgrass <64144934+osgrass@users.noreply.github.com>
2023-04-12 15:25:44 -0400

    Update ExpenseCategories.py

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -21,7 +21,7 @@ def main():
     print(timeit.timeit(stmt='expenses.categorize_for_loop()',
     setup = 
     '''
-import Expense
+from . import Expense
 expenses = Expense.Expenses()
 expenses.read_expenses('data/spending_data.csv')
     ''',
@@ -32,7 +32,7 @@ expenses.read_expenses('data/spending_data.csv')
     print(timeit.timeit(stmt='expenses.categorize_set_comprehension()',
     setup = 
     '''
-import Expense
+from . import Expense
 expenses = Expense.Expenses()
 expenses.read_expenses('data/spending_data.csv')
     ''',
